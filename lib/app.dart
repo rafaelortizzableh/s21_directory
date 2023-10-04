33f00bd5bdce8b5b211452fbb23f3509b9e0237a
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2023-10-04 15:44:01 +0200

    chore: add title to app

--- lib/app.dart
+++ lib/app.dart
@@ -6,10 +6,12 @@ import '../directory/directory.dart';
 class S21Directory extends StatelessWidget {
   const S21Directory({super.key});
 
+  static const _title = 'Directorio de Prestadores Médicos | S21';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Directorio de Prestadores',
+      title: _title,
       theme: _AppTheme._lightTheme,
       home: const ResponsiveLayout(),
       navigatorKey: AppConstants.navigatorKey,
